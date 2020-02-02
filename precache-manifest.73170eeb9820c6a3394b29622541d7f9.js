53fc14d433bd1a297ea38d202c9eaebd85fd5d0c
ssvas1997 <ssvas1997@gmail.com>
2020-02-02 16:14:27 +0530

    first commit

---
+++ precache-manifest.73170eeb9820c6a3394b29622541d7f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6b7e4c4915dd573afb2b49a13a8dc32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a64eb8aaad3bff84c019",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "0b4fbc1f56062f951901",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "a64eb8aaad3bff84c019",
+    "url": "/static/js/2.99c889ef.chunk.js"
+  },
+  {
+    "revision": "0b4fbc1f56062f951901",
+    "url": "/static/js/main.23c152f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
